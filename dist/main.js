6d095d2005c0be64e6e5cb31d5e5923c3abc1a5e
calvinpark <dev.calvinpark@gmail.com>
2022-12-21 17:06:46 +0900

    feat: validation

--- dist/main.js
+++ dist/main.js
@@ -3,9 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var zod_1 = require("zod");
 // Example of how zod works
 var UserSchema = zod_1.z.object({
-    // which is string
-    username: zod_1.z.string()
+    // which is string and it has to be longer than 3 characters
+    username: zod_1.z.string().min(3),
+    age: zod_1.z.number(),
+    birthday: zod_1.z.date(),
+    isPorgrammer: zod_1.z.boolean().optional(),
+    test: zod_1.z.undefined(),
+    // there are few other options such as 
+    // z.unknown(), z.null(), z.any(), etc...
+    never: zod_1.z.never()
 });
-var user = { user: 1 };
+var user = { user: "Calvin", age: 20, birthday: new Date(), isPorgrammer: true };
 console.log(UserSchema.parse(user));
-console.log("asdfasdf");
+// using safePaser for return to us an object which  has a success
+// field which will be true or false.
+// It is important if you want to do this for a form validation.
+// console.log(UserSchema.safeParse(user)) // false;
+// console.log(UserSchema.safeParse(user.success)) // true
+//console.log()
--- main.ts
+++ main.ts
@@ -3,20 +3,39 @@ import {z} from "zod";
 
 // Example of how zod works
 const UserSchema = z.object({
-    // which is string
-    username: z.string(),
-    age: z.number(),
+    // which is string and it has to be longer than 3 characters
+    username: z.string().min(3).max(20),
+    age: z.number().gt(0),
     birthday: z.date(),
-    isPorgrammer: z.boolean()
+    // nullable vs nullish
+    // nullish allows you to use undefined and null
+    // nullable only allows you to define null.
+    isPorgrammer: z.boolean().nullable(),
+    test: z.undefined(),
+    // there are few other options such as 
+    // z.unknown(), z.null(), z.any(), etc...
+    never: z.never(),
+    // this is a default value
+    default: z.boolean().default(true)
 });
 
 //automatically inferred the type. 
 type User = z.infer<typeof UserSchema>
 
-const user = {user: "Calvin"}
+const user = {
+    user: "Calvin",
+    age: 20, 
+    birthday: new Date(), 
+    isPorgrammer: null
+}
 
-console.log(UserSchema.parse(user));
+
+//console.log(UserSchema.parse(user))
+
+console.log(UserSchema.safeParse(user));
 // using safePaser for return to us an object which  has a success
 // field which will be true or false.
 // It is important if you want to do this for a form validation.
-console.log(UserSchema.safeParse(user))
+// console.log(UserSchema.safeParse(user)) // false;
+// console.log(UserSchema.safeParse(user.success)) // true
+
